3b775d5efc915adb2f9fcff640a264d0579d3bc6
Michael Dorst <m@mdorst.net>
2024-04-27 14:48:42 -0700

    Render a square

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    // Camera
+    commands.spawn(Camera2dBundle::default());
+
+    // Square
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::WHITE,
+            custom_size: Some(Vec2::new(100.0, 100.0)),
+            ..default()
+        },
+        ..default()
+    });
+
+    // Background color
+    commands.insert_resource(ClearColor(Color::BLACK));
 }
