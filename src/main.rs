f91ad67c756a9a92b7d9686ed4fa6362465011c6
Michael Dorst <m@mdorst.net>
2024-05-05 16:29:33 -0700

    Improve camera and play area configuration
    * Set the camera viewport to the width of the grid and put (0,0) at the bottom left
    * Refactor play area setup to use direct sizing
    * Remove redundant player calculations and event handling

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-use bevy::{
-    prelude::*,
-    window::{PrimaryWindow, WindowResized},
-};
+use bevy::{prelude::*, render::camera::ScalingMode};
 
-const GRID_HEIGHT: u8 = 18;
-const GRID_WIDTH: u8 = 32;
-
-#[derive(Debug, Component)]
-struct PlayArea(f32, f32);
+const GRID_WIDTH: f32 = 32.;
+const GRID_HEIGHT: f32 = 18.;
 
 #[derive(Component)]
 struct Player;
@@ -15,71 +9,51 @@ struct Player;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (setup_camera, setup_play_area, setup_clear_color))
-        .add_systems(Startup, spawn_player.after(setup_play_area))
-        .add_systems(Update, (resize_play_area, move_player))
+        .add_systems(
+            Startup,
+            (
+                setup_camera,
+                setup_play_area,
+                setup_clear_color,
+                spawn_player,
+            ),
+        )
+        .add_systems(Update, move_player)
         .run();
 }
 
 fn setup_camera(mut cmd: Commands) {
-    cmd.spawn(Camera2dBundle::default());
+    let mut camera = Camera2dBundle::default();
+    // Set the viewport to be 32 units wide
+    camera.projection.scaling_mode = ScalingMode::FixedHorizontal(GRID_WIDTH as f32);
+    // Pan the camera so that (0,0) is in the bottom left, assuming a 16x9 aspect ratio
+    camera.transform = Transform::from_xyz(GRID_WIDTH / 2. - 0.5, GRID_HEIGHT / 2. - 0.5, 0.);
+    cmd.spawn(camera);
 }
 
 fn setup_clear_color(mut cmd: Commands) {
     cmd.insert_resource(ClearColor(Color::GRAY));
 }
 
-fn setup_play_area(mut cmd: Commands, q: Query<&Window, With<PrimaryWindow>>) {
-    let res = &q.single().resolution;
-    cmd.spawn((
-        SpriteBundle {
-            sprite: Sprite {
-                color: Color::BLACK,
-                custom_size: Some(Vec2::new(0., 0.)),
-                ..default()
-            },
+fn setup_play_area(mut cmd: Commands) {
+    cmd.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::BLACK,
+            custom_size: Some(Vec2::new(GRID_WIDTH, GRID_HEIGHT)),
             ..default()
         },
-        get_play_area(res.width(), res.height()),
-    ));
+        // Move the play area so the bottom left corner is at (0,0)
+        transform: Transform::from_xyz(GRID_WIDTH / 2. - 0.5, GRID_HEIGHT / 2. - 0.5, 0.),
+        ..default()
+    });
 }
 
-fn resize_play_area(
-    mut q: Query<(&mut Sprite, &mut PlayArea)>,
-    mut resize_reader: EventReader<WindowResized>,
-) {
-    let (mut sprite, mut play_area) = q.single_mut();
-    for e in resize_reader.read() {
-        *play_area = get_play_area(e.width, e.height);
-        sprite.custom_size = Some(Vec2::new(play_area.0, play_area.1));
-    }
-}
-
-fn get_play_area(window_width: f32, window_height: f32) -> PlayArea {
-    if window_width < 16. * window_height / 9. {
-        PlayArea(window_width, 9. * window_width / 16.)
-    } else {
-        PlayArea(16. * window_height / 9., window_height)
-    }
-}
-
-fn spawn_player(mut cmd: Commands, q: Query<&PlayArea>) {
-    let play_area = q.single();
-    let (x, y) = (16, 9);
-    let cell_width = play_area.0 / GRID_WIDTH as f32;
-    let cell_height = play_area.1 / GRID_HEIGHT as f32;
-    let x_pos = -play_area.0 / 2. + (x as f32 + 0.5) * cell_width;
-    let y_pos = -play_area.1 / 2. + (y as f32 + 0.5) * cell_height;
-
+fn spawn_player(mut cmd: Commands) {
     cmd.spawn((
         SpriteBundle {
             sprite: Sprite {
                 color: Color::WHITE,
-                custom_size: Some(Vec2::new(cell_width, cell_height)),
-                ..default()
-            },
-            transform: Transform {
-                translation: Vec3::new(x_pos, y_pos, 1.),
+                custom_size: Some(Vec2::new(1., 1.)),
                 ..default()
             },
             ..default()
@@ -88,27 +62,15 @@ fn spawn_player(mut cmd: Commands, q: Query<&PlayArea>) {
     ));
 }
 
-fn move_player(
-    input: Res<ButtonInput<KeyCode>>,
-    mut xform_q: Query<&mut Transform, With<Player>>,
-    play_area_q: Query<&PlayArea>,
-) {
-    let (delta_x, delta_y) = if input.just_pressed(KeyCode::KeyW) {
-        (0., 1.)
+fn move_player(input: Res<ButtonInput<KeyCode>>, mut q: Query<&mut Transform, With<Player>>) {
+    let mut xform = q.single_mut();
+    if input.just_pressed(KeyCode::KeyW) {
+        xform.translation.y += 1.;
     } else if input.just_pressed(KeyCode::KeyA) {
-        (-1., 0.)
+        xform.translation.x -= 1.;
     } else if input.just_pressed(KeyCode::KeyS) {
-        (0., -1.)
+        xform.translation.y -= 1.;
     } else if input.just_pressed(KeyCode::KeyD) {
-        (1., 0.)
-    } else {
-        return;
-    };
-    let play_area = play_area_q.single();
-    let cell_width = play_area.0 / GRID_WIDTH as f32;
-    let cell_height = play_area.1 / GRID_HEIGHT as f32;
-
-    let mut xform = xform_q.single_mut();
-    xform.translation.x += delta_x * cell_width;
-    xform.translation.y += delta_y * cell_height;
+        xform.translation.x += 1.;
+    }
 }
