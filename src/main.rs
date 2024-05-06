b27c6b97f8667013a3d476d42ae95087e6e66e82
Michael Dorst <m@mdorst.net>
2024-05-05 17:40:02 -0700

    Player moves constantly in discrete steps

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,16 @@ use bevy::{prelude::*, render::camera::ScalingMode};
 
 const GRID_WIDTH: f32 = 32.;
 const GRID_HEIGHT: f32 = 18.;
+const STEPS_PER_SECOND: f32 = 4.;
 
 #[derive(Component)]
 struct Player;
+
+#[derive(Component)]
+struct Direction(Direction2d);
+
+#[derive(Resource)]
+struct MoveTimer(Timer);
 
 fn main() {
     App::new()
@@ -18,13 +25,16 @@ fn main() {
                 spawn_player,
             ),
         )
-        .add_systems(Update, move_player)
+        .add_systems(Update, (change_player_direction, move_player))
+        .insert_resource(MoveTimer(Timer::from_seconds(
+            1. / STEPS_PER_SECOND,
+            TimerMode::Repeating,
+        )))
         .run();
 }
 
 fn setup_camera(mut cmd: Commands) {
     let mut camera = Camera2dBundle::default();
-    // Set the viewport to be 32 units wide
     camera.projection.scaling_mode = ScalingMode::FixedHorizontal(GRID_WIDTH as f32);
     // Pan the camera so that (0,0) is in the bottom left, assuming a 16x9 aspect ratio
     camera.transform = Transform::from_xyz(GRID_WIDTH / 2. - 0.5, GRID_HEIGHT / 2. - 0.5, 0.);
@@ -59,18 +69,36 @@ fn spawn_player(mut cmd: Commands) {
             ..default()
         },
         Player,
+        Direction(Direction2d::X),
     ));
 }
 
-fn move_player(input: Res<ButtonInput<KeyCode>>, mut q: Query<&mut Transform, With<Player>>) {
-    let mut xform = q.single_mut();
+fn change_player_direction(
+    input: Res<ButtonInput<KeyCode>>,
+    mut q: Query<&mut Direction, With<Player>>,
+) {
+    let mut direction = q.single_mut();
+
     if input.just_pressed(KeyCode::KeyW) {
-        xform.translation.y += 1.;
+        direction.0 = Direction2d::Y;
     } else if input.just_pressed(KeyCode::KeyA) {
-        xform.translation.x -= 1.;
+        direction.0 = Direction2d::NEG_X;
     } else if input.just_pressed(KeyCode::KeyS) {
-        xform.translation.y -= 1.;
+        direction.0 = Direction2d::NEG_Y;
     } else if input.just_pressed(KeyCode::KeyD) {
-        xform.translation.x += 1.;
+        direction.0 = Direction2d::X;
+    }
+}
+
+fn move_player(
+    mut q: Query<(&mut Transform, &Direction), With<Player>>,
+    mut timer: ResMut<MoveTimer>,
+    time: Res<Time>,
+) {
+    timer.0.tick(time.delta());
+
+    if timer.0.finished() {
+        let (mut transform, direction) = q.single_mut();
+        transform.translation += direction.0.extend(0.);
     }
 }
