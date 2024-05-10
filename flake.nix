6684e425ec8d7b95b88dea8f165d84401a7e6564
Michael Dorst <m@mdorst.net>
2024-05-10 16:28:43 -0700

    Format flake

--- flake.nix
+++ flake.nix
@@ -7,33 +7,43 @@
 
   outputs = { self, nixpkgs, rust-overlay }:
     let
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+      systems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       overlays = [ (import rust-overlay) ];
 
       forAllSystems = fn:
         nixpkgs.lib.genAttrs systems
-          (system: fn { pkgs = import nixpkgs { inherit system overlays; }; });
+        (system: fn { pkgs = import nixpkgs { inherit system overlays; }; });
 
     in {
       devShells = forAllSystems ({ pkgs }: {
-        default = with pkgs; pkgs.mkShell rec {
-          name = "snake";
-          nativeBuildInputs = [
-            pkg-config
-          ];
-          buildInputs = [
-            rust-bin.stable.latest.default
-            udev alsa-lib vulkan-loader
-            xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
-            libxkbcommon wayland # To use the wayland feature
-          ];
-          LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
-        };
+        default = with pkgs;
+          pkgs.mkShell rec {
+            name = "snake";
+            nativeBuildInputs = [ pkg-config ];
+            buildInputs = [
+              cargo
+              clippy
+              rustc
+              rust-analyzer
+
+              udev
+              alsa-lib
+              vulkan-loader
+
+              # To use the x11 feature
+              xorg.libX11
+              xorg.libXcursor
+              xorg.libXi
+              xorg.libXrandr
+
+              # To use the wayland feature
+              libxkbcommon
+              wayland
+            ];
+            LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+          };
       });
+      formatter = forAllSystems ({ pkgs }: pkgs.nixfmt-classic);
     };
 }
