5ccbbb73fe89fcc8cd687ddd6301f1c4c61490fb
Michael Dorst <m@mdorst.net>
2024-06-15 14:25:27 -0700

    Make score a UI element with a background

--- src/score.rs
+++ src/score.rs
@@ -1,4 +1,4 @@
-use crate::{constants::*, food::EatEvent};
+use crate::food::EatEvent;
 use bevy::prelude::*;
 
 #[derive(Component, Default)]
@@ -14,28 +14,29 @@ impl Plugin for ScorePlugin {
 }
 
 fn setup_score_ui(mut cmd: Commands) {
-    let text_pos = Vec3::new(4.5, GRID_HEIGHT - 2., 0.);
-    let text_size = 0.025;
-
     let text_style = TextStyle {
-        font_size: 80.,
+        font_size: 40.,
         ..default()
     };
-    cmd.spawn((
-        Text2dBundle {
-            text: Text::from_sections([
-                TextSection::new("Score: ", text_style.clone()),
-                TextSection::new("0", text_style),
-            ]),
-            transform: Transform {
-                translation: text_pos,
-                scale: Vec3::splat(text_size),
-                ..default()
-            },
+
+    cmd.spawn(NodeBundle {
+        style: Style {
+            position_type: PositionType::Absolute,
+            left: Val::Px(10.),
+            top: Val::Px(10.),
+            padding: UiRect::all(Val::Px(5.)),
             ..default()
         },
-        Score(0),
-    ));
+        background_color: Color::rgba(0.0, 0.0, 0.0, 0.8).into(),
+        ..default()
+    })
+    .with_children(|parent| {
+        parent.spawn(TextBundle::from_sections([
+            TextSection::new("Score: ", text_style.clone()),
+            TextSection::new("0", text_style),
+        ]));
+    })
+    .insert(Score(0));
 }
 
 fn update_score_ui(mut ev_eat: EventReader<EatEvent>, mut q: Query<(&mut Text, &mut Score)>) {
